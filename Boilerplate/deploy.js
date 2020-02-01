653f734076a945038dffa6d41be32dfd51552d11
Zzocker <pkspritam10@gmail.com>
2020-02-01 23:08:35 +0530

    completed

--- Boilerplate/deploy.js
+++ Boilerplate/deploy.js
@@ -3,7 +3,7 @@ const Web3 = require('web3')
 const {interface,bytecode} = require('./compile')
 
 const provider= new Hdwallet(
-    'foot alone....' , 'https://rinkeby....'
+    'foot ...' , 'https://rinkeby.infura.io..a'
 )
 const web3 = new Web3(provider)
 
@@ -13,8 +13,8 @@ const deploy = async ()=>{
     const bal = await web3.eth.getBalance(accounts[0])
     console.log(bal)
     Inbox = await new web3.eth.Contract(interface) /// abi of contract
-    .deploy({data: bytecode,arguments:["InitHello"]})
-    .send({from:accounts[0],gas:'8000000'})
+    .deploy({data: '0x'+bytecode,arguments:["InitHello"]})
+    .send({from:accounts[0],gas:'1000000'})
     // console.log(Inbox)
     console.log(`contract address: ${Inbox.options.address}`)
 }
